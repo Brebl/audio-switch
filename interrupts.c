e77b5b982043a9da124a074110dc8a76118d90a9
Antti Helminen <keijo2@gsb.fi>
2021-06-01 22:31:13 +0300

    v0.2

--- interrupts.c
+++ interrupts.c
@@ -1,22 +1,31 @@
 #include "main.h"
+static uint8_t timeout_counter = 0;
+const uint8_t timeout_max = 5;
+static uint8_t led_counter = 0;
+const uint8_t led_max_count = 2;
+
 
 void init_interrupts()
 {
     GIMSK |= (1 << PCIE2);
     PCMSK2 |= (1 << PCINT16);
+    TCCR0A |= (1 << WGM01);
+    TCCR0B |= (1 << CS00) | (1 << CS02);
+    TIMSK |= (1 << OCIE0A);
+    OCR0A = 0xff;
     sei();
 }
 
 ISR(PCINT2_vect) {
-    static uint8_t led_counter = 0;
-    const uint8_t led_max_count = 2;
-    uint8_t buttonpins;
-    buttonpins = debounce(PIND);
+    uint8_t buttonpins = debounce(PIND);
     if(bit_is_clear(buttonpins, PD5)){
-        led_counter++;
+        if(PORTB != 0) {
+            led_counter++;
+        }
         if(led_counter > led_max_count) {
             led_counter = 0;
         }
+        timeout_counter = 0;
         switch (led_counter) {
         case 0:
             PORTB = led1;
@@ -33,6 +42,15 @@ ISR(PCINT2_vect) {
             buttonpins = debounce(PIND);
             _delay_ms(5);
         }        
-    }
-    
+    }  
 }  
+
+ISR(TIMER0_COMPA_vect)
+{
+    timeout_counter++;
+    if(timeout_counter >= timeout_max) {
+        timeout_counter = 0;
+        PORTB = 0;
+    }
+
+}
