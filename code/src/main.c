23b0b0b0a9ad0abd5bddb732245fdc490c13bec6
Antti Helminen <antti@gsb.fi>
2021-11-20 21:54:09 +0200

    added: display

--- code/src/main.c
+++ code/src/main.c
@@ -1,12 +1,12 @@
 #include "main.h" 
+#include "display.h"
+
+extern inline void transistors_off();
 
 int main(void)
 {   
-    //init ports
-    DDRA = (1 << PA0);
-    DDRB = (1 << PB0) | (1 << PB1) | (1 << PB2) | (1 << PB3) | (1 << PB4);
-    DDRD = (1 << PD0) | (1 << PD1) | (1 << PD2) | (1 << PD3) | (1 << PD4) | (1 << PD5);
-    PORTD = (1 << PUSH_BUTTON);
+    INIT;
+    LCD_init();
     init_interrupts();
 
     while (1) {
