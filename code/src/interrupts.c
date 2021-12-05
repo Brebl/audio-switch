ef9291c42f085ab611048a7ac016cb5f76626fba
Antti Helminen <antti@gsb.fi>
2021-12-05 14:46:42 +0200

    added: spi sceleton

--- code/src/interrupts.c
+++ code/src/interrupts.c
@@ -1,8 +1,8 @@
 #include "main.h"
 #include "display.h"
-static uint8_t timeout_counter = 0;
+volatile static uint8_t timeout_counter = 0;
 const uint8_t timeout_max = 10;           //secs
-static uint8_t audioport_counter = 0;
+volatile static uint8_t audioport_counter = 0;
 const uint8_t audioport_max = 5;
 
 void init_interrupts()
@@ -20,11 +20,14 @@ void init_interrupts()
     == 1Hz
     **********************/
     GIMSK |= (1 << PCIE2);
-    PCMSK2 |= (1 << PCINT17);
+    PCMSK2 |= (1 << PCINT17);               //push button
     TCCR0A |= (1 << WGM01);                 //ctc
     TCCR0B |= (1 << CS00) | (1 << CS02);    //1024 prescaler
     OCR0A = 0x7d;                           //125
     TIMSK |= (1 << OCIE0A);                 //ocr-a enable
+    DDRB |= (1 << PB6);                     //output DO
+    PORTB |= (1 << PB5) | (1 << PB7);       //input DI, SCK
+    USICR = 0x98;                           //10011000 = USISIE; wiremode:spi; clocksrc:ext-pos.edge
     sei();
     set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     LCD_string(AUDIO1);
@@ -91,4 +94,41 @@ ISR(TIMER0_COMPA_vect)
         LCD_AK(0);
         set_sleep_mode(SLEEP_MODE_PWR_DOWN);
     }
+}
+
+ISR(USI_START_vect) //SPI
+{
+    //USIDR = 0xff; //if you wish to send byte to master
+    USISR = (1 << USIOIF); //clear overflow flag and counter
+    while (!(USISR & (1 << USIOIF))) {;}
+    transistors_off();
+    switch (USIDR) {
+    case 0:
+        TRAN1(1);
+        LCD_animate(AUDIO1);
+        break;
+    case 1:
+        TRAN2(1);
+        LCD_animate(AUDIO2);
+        break;
+    case 2:
+        TRAN3(1);
+        LCD_animate(AUDIO3);
+        break;
+    case 3:
+        TRAN4(1);
+        LCD_animate(AUDIO4);
+        break;
+    case 4:
+        TRAN5(1);
+        LCD_animate(AUDIO5);
+        break;
+    case 5:
+        TRAN6(1);
+        LCD_animate(AUDIO6);
+        break;
+    default:
+        break;
+    }
+    USISR = (1 << USISIF); //clear interrupt flag
 }
